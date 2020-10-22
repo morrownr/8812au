a300491804dc3186e9efc7075216293f2e986ab0
morrownr <morrownr@gmail.com>
2020-10-21 22:02:21 -0500

    usb 3 support

--- dkms-remove.sh
+++ dkms-remove.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+DRV_NAME=rtl8812au
+DRV_VERSION=5.6.4.2
+
 if [[ $EUID -ne 0 ]]; then
   echo "You must run this installation script with superuser priviliges."
   echo "Try \"sudo ./dkms-remove.sh\""
   exit 1
 fi
 
-DRV_NAME=rtl8812au
-DRV_VERSION=5.6.4.2
-
 dkms remove ${DRV_NAME}/${DRV_VERSION} --all
-rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
-
 RESULT=$?
 
 if [[ "$RESULT" != "0" ]]; then
-  echo "An error occurred while running dkms-remove.sh."
+  echo "An error occurred while running dkms remove"
+  exit $RESULT
 else
-  echo "dkms-remove.sh was successful."
+	echo ""
+	echo "Removing /etc/modprobe.d/8812au.conf"
+	rm -f /etc/modprobe.d/8812au.conf
+	echo ""
+	echo "Removing /usr/src/${DRV_NAME}-${DRV_VERSION)"
+	rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
+	echo "dkms-remove.sh was successful."
 fi
-
-exit $RESULT

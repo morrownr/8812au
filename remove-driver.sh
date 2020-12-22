4c171fc52e48cb40c483bc7584026c7c52f56e98
morrownr <morrownr@gmail.com>
2020-12-22 11:43:29 -0600

    minor updates

--- remove-driver.sh
+++ remove-driver.sh
@@ -4,7 +4,8 @@ DRV_NAME=rtl8812au
 DRV_VERSION=5.9.3.2
 OPTIONS_FILE=8812au.conf
 
-SCRIPT_NAME=remove-driver.sh
+KRNL_VERSION=$(uname -r)
+SCRIPT_NAME="remove-driver.sh"
 
 if [[ $EUID -ne 0 ]]; then
 	echo "You must run this script with superuser (root) privileges."
@@ -19,9 +20,9 @@ dkms remove ${DRV_NAME}/${DRV_VERSION} --all
 RESULT=$?
 
 if [[ "$RESULT" != "0" ]]; then
-	echo "An error occurred while running dkms remove : dkms return code: ${RESULT}"
+	echo "An error occurred while running: dkms remove : ${RESULT}"
 	exit $RESULT
 else
-	echo "The module has been removed successfully."
+	echo "The driver was removed successfully."
 	exit 0
 fi
